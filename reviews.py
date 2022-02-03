d17515ce1df363ca70bd3e1b6e56ecd11ee26925
hasnainjafri15 <68164874+hasnainjafri15@users.noreply.github.com>
2022-02-03 08:45:35 +0500

    second commit

--- reviews.py
+++ reviews.py
@@ -11,44 +11,54 @@ import pandas as pd
 import numpy as np
 from selenium.webdriver.common.action_chains import ActionChains
 
-
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://play.google.com/store/apps/developer?id=Blink+Co.")
 
-reviews_list = []
+time.sleep(22)
 
+#reviews_list = []
+
+df_price = pd.DataFrame()
 
 x = 0
-for x in range(0,3): 
+while x in range(20):
+    reviews_list = []
     x = x + 1
-    app_link = driver.find_element_by_xpath("/html/body/div[1]/div[4]/c-wiz/div/div[2]/c-wiz/c-wiz/c-wiz/div/div[2]/div[{}]/c-wiz/div/div/div[2]/div/div/div[1]/div/div/div[1]/a/div".format(x))
-    app_link.click()
-
-    time.sleep(2)
-
-    #name = driver.find_element_by_xpath(
-        #'/html/body/div[1]/div[4]/c-wiz[3]/div/div[2]/div/div/main/c-wiz/c-wiz[1]/div/div[2]/div/div[1]/c-wiz[1]/h1/span').text
-    
-
     try:
-        y = 1
-        for y in range(1,3):
-            y = y + 1
-            reviews = driver.find_element_by_xpath("/html/body/div[1]/div[4]/c-wiz[2]/div/div[2]/div/div/main/div/div[1]/div[{}]/div/div[2]/div[2]/span[1]".format(y)).text
-            name = []  #name should be that of name
-            name.append(reviews)
-            r_r = pd.DataFrame(name, columns=[name])
-            
+        app_link = driver.find_element_by_xpath('/html/body/div[1]/div[4]/c-wiz/div/div[2]/c-wiz/c-wiz/c-wiz/div/div[2]/div[{}]/c-wiz/div/div/div[2]/div/div/div[1]/div/div/div[1]/a/div'.format(x))
+        app_link.click()
     except:
-        reviews_list.append('none')
+     pass
 
     time.sleep(2)
+
+    #name = driver.find_element_by_class_name('AHFaub').text
+
+    list = [2, 3, 4, 5, 6]
+
+    for y in list:
+           #y = y + 1
+           #print(x)
+           #time.sleep(2)
+           #print(y)
+           #driver.execute_script("window.scrollBy(0,3000)", "")
+           try:
+                reviews = driver.find_element_by_xpath(
+                    '/html/body/div[1]/div[4]/c-wiz[2]/div/div[2]/div/div/main/div/div[1]/div[{}]/div/div[2]/div[2]/span[1]'.format(y)).text
+                reviews_list.append(reviews)
+            except:
+                reviews_list.append('N/A')
 
     driver.back()
+    time.sleep(2)
+
+    df_price[x] = pd.DataFrame(reviews_list)
+    #df_price['sentiment'] = pd.DataFrame()
+
 
 driver.quit()
 
-print(r_r)
 
+df_price()
